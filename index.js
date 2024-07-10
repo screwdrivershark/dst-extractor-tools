6f8672badebc3187248c5968c2b551c1c86fa2ce
curious-broccoli <77789413+curious-broccoli@users.noreply.github.com>
2024-07-10 02:08:50 +0200

    style and get file text

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
 "use strict";
 
+const fileNameRegex = /speech_(.*)\.lua/;
 console.log("start");
 const inputElement = document.querySelector("#file-input");
+// if I handle the case where files are already selected, I won't have to clear the input
+inputElement.value = null;
 inputElement.addEventListener("change", handleFilesChanged);
 
+/**
+ * @param {Array<File>} files 
+ */
+function readFiles(files) {
+    const fileData = {};
+
+    files.forEach((file) => {
+        const characterName = getCharacterNameFromFileName(file.name);
+        fileData[characterName] = characterName;
+    });
+    console.log(fileData);
+}
+
+/**
+ * @param {String} fileName must fit the format "speech_name.lua"
+ * @returns {String} the character name as used in the game files
+ */
+function getCharacterNameFromFileName(fileName) {
+    const match = fileNameRegex.exec(fileName);
+    if (match) {
+        return match[1];
+    } else {
+        throw Error(`Couldn't find a character name in the file name '${fileName}'`);
+    }
+}
+
+/**
+ * @param {Event} e
+ */
 async function handleFilesChanged(e) {
-    const files = this.files;
+    const files = Array.from(this.files);
     if (files.length === 0) {
-        console.debug("No files given, returning early...");
+        console.debug("Zero files given, returning early...");
         return;
     }
 
     console.log(files);
-    const inputElement = document.querySelector("#file-input");
-    inputElement.addEventListener("change", handleFiles);
-
-    files[0].text().then(data => console.log(data));
+    const fileData = readFiles(files);
 }
 
+/**
+ * @param {String} error 
+ */
 function showError(error) {
     const errorElement = document.querySelector("#error");
     errorElement.textContent = error;
